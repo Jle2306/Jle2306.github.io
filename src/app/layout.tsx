3369d0c366dd1dc33f86ccf0994f603b1f4ce480
Jle2306 <Jle2306@users.noreply.github.com>
2026-05-28 16:09:27 +0800

    Revert "feat: 添加后台访问统计系统"
    This reverts commit 0253ab466bd5b521f8224085aeee741045cf4fd5.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { AnalyticsProvider } from "@/components/AnalyticsProvider";
 import { Footer } from "@/components/layout/Footer";
 import { Header } from "@/components/layout/Header";
 import { siteConfig } from "@/config/site";
@@ -50,11 +49,9 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="flex min-h-full flex-col">
-        <AnalyticsProvider>
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-        </AnalyticsProvider>
+        <Header />
+        <main className="flex-1">{children}</main>
+        <Footer />
       </body>
     </html>
   );
